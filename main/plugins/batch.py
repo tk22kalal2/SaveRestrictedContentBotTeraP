f0c8b6fa2e6adbd863bc20cc7695ddb824c42133
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-21 21:40:34 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -109,8 +109,6 @@ async def run_batch(userbot, client, sender, link, _range):
                     break
                 await asyncio.sleep(fw.x + 5)
                 t = await get_bulk_msg(userbot, client, sender, link, i)
-                try:
-                    await t.forward(chat_id=DB_CHANNEL)
             protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
             await asyncio.sleep(timer)
             await protection.delete()
