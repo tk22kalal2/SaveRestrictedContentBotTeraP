83974a738c7df34a797a7caa0444a7977c0d1143
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-21 20:23:15 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 28698132
-API_HASH = "1a2837cd39f87e9b224710af8acd13de"
-BOT_TOKEN = "6703992527:AAGhh3nJssR-OFN5p1jnn3LhflP_lErMdj0"
+API_ID = 24058425
+API_HASH = "694b063e55c24287a3d30aed90191373"
+BOT_TOKEN = "6802436912:AAG8YFXKZZMduxzHAOFTqS0b87qAwrjpoO0"
 SESSION = "BQAs5elmRJKYoKFqIfOUUpyCIDydkDVmPYBudI5Rc9G1gavJTfbqSSoUkO94UcG2QXjfU4aVsp3GTIl4TpTCCglH7P0fwngL9IbfCfji3AxyeQfLifEhhXAaf1Ql-sVPv5X-9iw2NQ_9uNg7szzYspcOZmqpdlJzdkNVx2_6oEOAiQWhYjr2S769JxJzYn11huhHodyBDaKxfv_a6Y_TF-mACAgw6G3lgAKzHQMpJHDnkJROSIktuVqeK4LqJV6fIlfm81WC3dFgGOnHbHnOO-5l2MwTCF_kS5SHPqNrAcMD9oj1HmVXTpoOm7o0XZdrTYeeG7K8E5glnS8A2ci0ZKsWAAAAAaAj3_oA"
-FORCESUB = "forcesubpavo2"
-AUTH = 6981672954
+FORCESUB = "forcesubpavo3"
+AUTH = 6356781743
 DB_CHANNEL = -1002039233060
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
