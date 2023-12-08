335ba467cc9b173916b85ab8b94404b1aca2ca37
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-12-08 15:17:36 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -103,7 +103,7 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
                 except Exception:
                     thumb_path = None
                 await client.send_video(
-                    chat_id=sender,
+                    chat_id=DB_CHANNEL,
                     video=file,
                     caption=caption,
                     supports_streaming=True,
